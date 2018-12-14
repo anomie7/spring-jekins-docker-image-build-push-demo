e09dde9ff89460ceb74548fd2ef8d8943b9e11ec
Justin <anomie7777@gmail.com>
2018-12-14 16:41:53 +0900

    init

---
+++ src/main/java/com/example/demo/JenkinsDockerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerDemoApplication.class, args);
+	}
+
+}
+
